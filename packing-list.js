7a5046131b760a1a6dcafe496a491300315b193c
ahrke <david.duy.minh@gmail.com>
2019-07-02 08:14:51 +0000

    Added while loop to iterate through packing list

--- packing-list.js
+++ packing-list.js
@@ -4,3 +4,10 @@ console.log("Kitchen stuff to pack:");
 for(let i = 0; i < packingList.length; i++){
   console.log(packingList[i])
 }
+
+let i = 0;
+console.log("\nKitchen stuff to pack: (while loop)")
+while(i < packingList.length){
+  console.log(packingList[i])
+  i++
+}
